4c079bad4f2ae419e18f9a81cb99ce742852f945
denis25062002 <denis25062002@yandex.ru>
2023-07-07 11:27:48 +0700

    string to message

--- data_to_tx.h
+++ data_to_tx.h
@@ -1,8 +1,18 @@
 #ifndef DATA_TO_TX
 #define DATA_TO_TX
 
+typedef enum DTX_State
+{
+    WAIT_T = 0,
+    WAIT_XS,
+    COPY_PAYLOAD,
+    WAIT_LF,
+} DTX_State;
+
 typedef struct DataToTX
 {
+    DTX_State state;
+    size_t tx_buf_pointer;
     void(*send_tx) (void *state, char* tx_buf, size_t tx_buf_lenght);
     char* tx_buf;
     size_t tx_buf_lenght;
