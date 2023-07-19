4cf86f2abcc1693015334194b274cbc008722d2c
denis25062002 <denis25062002@yandex.ru>
2023-07-19 11:42:40 +0700

    tx server test 2 fix

--- test_tx_server_2.c
+++ test_tx_server_2.c
@@ -15,14 +15,14 @@ void get_data_test(unsigned char id, unsigned char* args, size_t args_lenght,
     unsigned char* responce, size_t* responce_lenght, unsigned char* error)
 {
     *error = 0;
-    unsigned char* test_responce[] = "6789";
+    unsigned char test_responce[256] = "6789";
     printf("\nID: %X\nArgs: ", id);
     for (size_t i = 0; i < args_lenght; i++)
     {
         printf("%c", args[i]);
     }
     memcpy(responce, test_responce, 4);
-    responce_lenght = 4;
+    *responce_lenght = 4;
 }
 
 
@@ -34,5 +34,5 @@ void main()
     char tx_buf[256];
     size_t tx_buf_lenght;
     TxSrv_init(&server, 0x21, responce_func_test, get_data_test, tx_buf, tx_buf_lenght);
-    TxSrv_write_data(&server, *test_input, 16);
+    TxSrv_write_data(&server, test_input, 16);
 }
